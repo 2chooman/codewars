b160a5ea63a4c8a4d38dc6cf396726eb5141d663
2chooman <2chooman@protonmail.com>
2020-08-19 20:11:08 +0300

    Create v1.js

---
+++ is_a_number_prime/v1.js
@@ -0,0 +1,13 @@
+// is_a_number_prime https://www.codewars.com/kata/5262119038c0985a5b00029f
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+        i += 6;
+    };
+    return true;
+}
